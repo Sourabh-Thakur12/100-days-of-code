3b6200d6fce27654ce3e4c6973ba7033c15b80b2
Sourabh Thakur12 <sourabhthakur0423@gmail.com>
2024-03-21 23:57:11 +0530

    upadating daily logs

---
+++ code-logs/day4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    *a+=*b;
+    if(*a>*b)
+    {*b=*a-*b;}
+    else
+     *b=*b-*a;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
